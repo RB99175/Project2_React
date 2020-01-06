d85f3efbd38c7926a2a4e582bcb94bf200c79f2b
Bhandarkar Roopali <bhandarkarRoopalit@johndeere.com>
2020-01-05 20:52:19 -0600

    FirstPage

--- formula1/src/Component/Button.js
+++ formula1/src/Component/Button.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../App.css";
 
+
+//Create two button:
+//one for year input and another one to click it to navigate to another page
 const Button= ({name, onclick}) => {
     
   return (
--- formula1/src/Component/YearDropDown.js
+++ formula1/src/Component/YearDropDown.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../App.css";
 
-const YearDropDown = ({seasons}) => {
-    console.log(seasons);
+//Adding the data from setstate(seasons) in yeardropdown button
+const YearDropDown = ({ seasons }) => {
+  //console.log to fetch all seasons detials
+  console.log(seasons);
+
   return (
     <select>
+      {/* Using map function to call each item in a string and returing those in results */}
       {seasons.map(seasonItem => (
         <option value={seasonItem.seasons}>{seasonItem.season}</option>
       ))}
